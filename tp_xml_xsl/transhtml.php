cfd137221fbf24720c08c103d8b3639f91ab261e
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-11-14 18:48:23 +0100

    tp2 question 4.4, error class XSLT not found

---
+++ tp_xml_xsl/transhtml.php
@@ -0,0 +1,12 @@
+<?php
+    $xslDoc = new DOMDocument();
+    $xslDoc->load($_GET["xsl"]);
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->load($_GET["xsl"]);
+    $xmlDoc->xinclude();
+    $proc = new XSLTProcessor();
+    $proc->importStylesheet($xslDoc);
+    $fichierHTML = fopen($_GET["html"], "w");
+    fwrite($fichierHTML, $proc->transformToXML($xmlDoc));
+    fclose($fichierHTML);
+?>
