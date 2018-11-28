f4a931656d618138cd06a4ac4316432f9e743f4f
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-11-28 18:05:53 +0100

    Affichage du voilier qui a le focus dans le form

---
+++ tp_ajax/scripts/Voiliers_voir_v2.js
@@ -0,0 +1,38 @@
+window.onload = initPage;
+     
+function initPage() {
+    peupleChoix("Voiliers.xml");
+    document.getElementById("choix").onchange = afficheSelection;
+}
+
+function peupleChoix(url) {
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = callback_peupleChoix;
+    xhr.open("GET", url);
+    xhr.send(null);
+}
+
+function callback_peupleChoix() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        tabNoms = xhr.responseXML.getElementsByTagName("nom");
+        div = document.getElementById("choix");// we take the form
+
+        for (i = 0; i != tabNoms.length; i++) {
+            nomVoilier = tabNoms[i].firstChild.nodeValue;
+        
+            elt = document.createElement("option");
+            noeudTexte = document.createTextNode(nomVoilier);
+        
+            elt.appendChild(noeudTexte);div.appendChild(elt);
+        }
+    }
+}
+
+function afficheSelection () {
+    if (n = document.getElementById("selection").firstChild)
+        document.getElementById("selection").removeChild(n);
+	indice = document.getElementById("choix").selectedIndex;
+	nomVoilier = document.getElementById("choix").value;
+    // on prend le nom du voilier en fonction du choix
+	document.getElementById("selection").appendChild(document.createTextNode(indice + 1 + " - " + nomVoilier));
+} //afficheSelection ()
